b7d1aead063cc9ba168c164af0e7009bd0cc9c8b
Tee <trung.lamkhanh2907@hcmut.edu.vn>
2024-11-26 19:59:22 +0700

    Upload test file

---
+++ base/test_connections.py
@@ -0,0 +1,23 @@
+import pyodbc
+import os
+
+db_name = os.getenv('DB_NAME', 'amazon_sales')
+db_user = os.getenv('DB_USER', 'azure_sa')
+db_password = os.getenv('DB_PASSWORD', '@123456A')
+db_host = os.getenv('DB_HOST', 'amazon-sql-server.database.windows.net')
+db_port = os.getenv('DB_PORT', '1433')
+
+try:
+    # Cố gắng kết nối tới SQL Server
+    conn = pyodbc.connect(
+        f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+        f'SERVER={db_host},{db_port};'
+        f'DATABASE={db_name};'
+        f'UID={db_user};'
+        f'PWD={db_password};'
+        'TrustServerCertificate=yes;'
+        'Connection Timeout=30;'
+    )
+    print("Kết nối thành công!")
+except Exception as e:
+    print(f"Không thể kết nối: {str(e)}")
